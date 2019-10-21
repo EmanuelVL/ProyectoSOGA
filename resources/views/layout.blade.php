8c21090e2c0e2bbfa8069c8350cef709bb1d35c5
Emanuel <emanuel.205m@gmail.com>
2019-10-20 19:58:46 -0700

    Se crea archivo plantilla con la base del sidebar para las rutas

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div class="sidenav">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+        </div>
+    </body>
+</html>
