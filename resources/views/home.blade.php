52c61bfab64ef8952a65e8ef3a36bff59904b4e8
Hugo Eguino <a217201885@alumnos.unison.mx>
2019-10-22 21:14:41 -0700

    Plantilla

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Home</title>
-    </head>
-    <body>
-        <h1>Home</h1>
-    </body>
-</html>
+@extends('base')
+
+@section('secion')
+
+
+@endsection
