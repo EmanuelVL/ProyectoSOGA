64a7ac6b9a36f605fddad154f133c2b08630a4ab
Emanuel <emanuel.205m@gmail.com>
2019-10-27 23:39:03 -0700

    Login lleva a menú

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 Route::post('home', 'Auth\LoginController@login')->name('login');
-=======
 Route::view('/', 'auth/login')->name('home');
 
 Auth::routes();
->>>>>>> bce2358cebf89240289ec327d6ebdc41373137eb
 
 Route::get('/home', 'HomeController@index')->name('home');
 
@@ -15,3 +12,4 @@ Route::get('register','HomeController@agregar')->name('ventana4');
 Route::post('register','HomeController@agregarus')->name('agrgarusua');
 
 Route::view('menu', 'menu')->name('menu');
+Route::post('menu', 'HomeController@index')->name('menu');
