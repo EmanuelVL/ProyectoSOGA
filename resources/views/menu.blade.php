0e4ea68f0725268ac812e197be3b8d2b95aac3d9
Emanuel <emanuel.205m@gmail.com>
2019-10-27 17:42:09 -0700

    Vista para el menú de áreas

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.template')
+
+@section('title', 'Menú')
+
+@section('style')
+    .button {
+      background-color: #4CAF50;
+      border: none;
+      color: white;
+      padding: 50px 50px;
+      text-align: center;
+      font-size: 16px;
+      cursor: pointer;
+    }
+
+    .button:hover {
+      background-color: green;
+    }
+
+    html, body {
+        width: 100%;
+    }
+    table {
+        margin: 0 auto;
+        margin-top: 13%;
+    }
+@endsection
+
+@section('content')
+    <table style="width:75%">
+    <tr>
+      <th> <button class="button">Área 1</button> </th>
+      <th> <button class="button">Área 2</button> </th>
+      <th> <button class="button">Área 3</button> </th>
+      <th> <button class="button">Área 4</button> </th>
+    </tr>
+    <tr>
+      <th> <button class="button">Área 5</button> </td>
+      <th> <button class="button">Área 6</button> </td>
+      <th> <button class="button">Área 7</button> </td>
+      <th> <button class="button">Área 8</button> </td>
+    </tr>
+    </table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,6 @@ Route::get('register','HomeController@agregar')->name('ventana4');
 
 Route::post('register','HomeController@agregarus')->name('agrgarusua');
 
+Route::view('menu', 'menu')->name('menu');
+
 //Route::get('/','HomeController@verAreas')->name('verAreas');
